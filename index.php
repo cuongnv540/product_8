a380c8b62fe52c0261d669eb8490482d30a8f49b
cuongnv540 <catbui7583@gmail.com>
2014-08-26 23:05:23 +0700

    3 commit

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 $yii = dirname(__FILE__).'/1.1.10/yii.php';
 $config = dirname(__FILE__).'/protected/config/home.php';
 // Remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 require_once($yii);
 Yii::createWebApplication($config)->runEnd('home');
